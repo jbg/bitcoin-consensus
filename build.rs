a7ea5fcb793df4d6bd962d68b6f3b4a80b328d9a
Jasper Bryant-Greene <jbg@rf.net.nz>
2019-07-20 19:04:39 +0100

    Clone bitcoin-core in the build script instead of using a submodule

--- build.rs
+++ build.rs
@@ -1,16 +1,22 @@
 extern crate num_cpus;
 
-use std::process::Command;
 use std::env;
+use std::path::PathBuf;
+use std::process::Command;
+
 
 fn main() {
-//    Command::new("git").args(&["submodule", "update", "--init"]).status().unwrap();
-    env::set_current_dir("bitcoin-core").unwrap();
+    let pkg_version = env::var("CARGO_PKG_VERSION").unwrap();
+    let bitcoin_version = pkg_version.split('+').skip(1).next().unwrap();
+    let out_dir = env::var("OUT_DIR").unwrap();
+    env::set_current_dir(&out_dir).unwrap();
+    let tag = format!("v{}", bitcoin_version);
+    Command::new("git").args(&["clone", "-b", tag.as_str(), "https://github.com/bitcoin/bitcoin"]).status().unwrap();
+    let bitcoin_build_dir: PathBuf = [out_dir.as_str(), "bitcoin"].iter().collect();
+    env::set_current_dir(&bitcoin_build_dir).unwrap();
 
     Command::new("sh").arg("./autogen.sh").status().unwrap();
-    let out_dir = env::var("OUT_DIR").unwrap();
     Command::new("sh").args(&["./configure",
-                              &format!("--prefix={}", out_dir),
                               "--with-gui=no",
                               "--with-daemon=no",
                               "--with-utils=no",
@@ -21,9 +27,6 @@ fn main() {
                               "--with-miniupnpc=no"]).status().unwrap();
     let cpus = num_cpus::get();
     Command::new("make").arg(format!("-j{}", cpus)).status().unwrap();
-    Command::new("make").args(&[&format!("-j{}", cpus), "install"]).status().unwrap();
-    env::set_current_dir("src/secp256k1").unwrap();
-    Command::new("make").args(&[&format!("-j{}", cpus), "install"]).status().unwrap();
 
     let target = env::var("TARGET").unwrap();
     if !target.contains("msvc") {
@@ -31,7 +34,8 @@ fn main() {
         println!("cargo:rustc-link-lib=dylib={}", cpplib);
     }
 
-    println!("cargo:rustc-link-search=native={}/lib", out_dir);
+    println!("cargo:rustc-link-search=native={}/src/.libs", bitcoin_build_dir.to_str().unwrap());
+    println!("cargo:rustc-link-search=native={}/src/secp256k1/.libs", bitcoin_build_dir.to_str().unwrap());
     println!("cargo:rustc-link-lib=static=bitcoinconsensus");
     println!("cargo:rustc-link-lib=static=secp256k1");
 }
